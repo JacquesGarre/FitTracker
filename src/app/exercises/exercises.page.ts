f5f4fce1d9dd80fd4f04d63466619ab0afb5e656
Jacques Garré <jcqs.gr@gmail.com>
2023-12-31 17:28:18 +0100

    filters wip

--- src/app/exercises/exercises.page.ts
+++ src/app/exercises/exercises.page.ts
@@ -9,6 +9,7 @@ import { Observable } from 'rxjs';
 import { Exercise } from '../exercise';
 import { RouterLink, ActivatedRoute, Router, RouterLinkActive, NavigationEnd } from '@angular/router';
 import { ExerciseCardComponent } from '../exercise-card/exercise-card.component';
+import { IonInput, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
 
 @Component({
     selector: 'app-exercises',
@@ -21,6 +22,12 @@ export class ExercisesPage implements OnInit {
 
     exercises!: Exercise[];
     loading: boolean = true;
+    textSearch: string = '';
+    typeSearch: string[] = [];
+    muscleGroupSearch: string[] = [];
+    difficultySearch: number[] = [];
+    types: string[] = [];
+    muscleGroups: string[] = [];
 
     constructor(
         private api: ApiService
@@ -28,6 +35,18 @@ export class ExercisesPage implements OnInit {
         this.api.getExercises(1).subscribe(
             (data: Exercise[]) => {
                 this.exercises = data;
+                for(const exercise of this.exercises){
+                    for(const type of exercise.type){
+                        if(!this.types.includes(type.title)){
+                            this.types.push(type.title)
+                        }
+                    }
+                    for(const muscleGroup of exercise.muscleGroups){
+                        if(!this.muscleGroups.includes(muscleGroup.title)){
+                            this.muscleGroups.push(muscleGroup.title)
+                        }
+                    }
+                }
                 this.loading = false;
             }
         );
@@ -35,6 +54,78 @@ export class ExercisesPage implements OnInit {
 
     ngOnInit() {
         
+    }
+
+    matchFilters(exercise: Exercise) {
+
+        let matchType = this.typeSearch.length < 1;
+        let matchMuscleGroup = this.muscleGroupSearch.length < 1;
+        let matchTitle = this.textSearch.length < 1;
+        let matchDifficulty = this.difficultySearch.length < 1;
+
+        // by type
+        if(this.typeSearch.length){
+            for(const type of this.typeSearch){
+                for(const exType of exercise.type){
+                    if(exType.title.toLowerCase().includes(type.toLowerCase())){
+                        matchType = true;
+                        break;
+                    }
+                }
+                if(matchType){
+                    break;
+                }
+            }
+        }
+
+        // by muscle group
+        if(this.muscleGroupSearch.length){
+            for(const muscleGroup of this.muscleGroupSearch){
+                for(const group of exercise.muscleGroups){
+                    if(group.title.toLowerCase().includes(muscleGroup.toLowerCase())){
+                        matchMuscleGroup = true;
+                        break;
+                    }
+                }
+                if(matchMuscleGroup){
+                    break;
+                }
+            }
+        }
+
+        // by difficulty
+        if(this.difficultySearch.length){
+            for(const difficulty of this.difficultySearch){
+                if(exercise.difficulty == difficulty){
+                    matchDifficulty = true;
+                    break;
+                }
+            }
+        }
+        
+        // by text
+        if(this.textSearch.length){
+            matchTitle = exercise.title.toLowerCase().includes(this.textSearch.toLowerCase());
+
+            if(!matchTitle){
+                for(const group of exercise.muscleGroups){
+                    if(group.title.toLowerCase().includes(this.textSearch.toLowerCase())){
+                        matchTitle = true;
+                        break;
+                    }
+                }
+            }
+            if(!matchTitle){
+                for(const type of exercise.type){
+                    if(type.title.toLowerCase().includes(this.textSearch.toLowerCase())){
+                        matchTitle = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return matchTitle && matchMuscleGroup && matchType && matchDifficulty;
     }
 
 }
